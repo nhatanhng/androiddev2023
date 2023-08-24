757f8589db19cc0e9d4879a37b2f69948b0ecf14
nhatanhng <nguyennhatanh0810@gmail.com>
2023-08-24 11:04:13 +0700

    lab2

---
+++ USTHweather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,54 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("","onCreate() Message on.");
+
+    }
+    @Override
+    protected void onResume() {
+// Always call the superclass method first
+        super.onResume();
+        Log.i("","onResume() Message on.");
+
+    }
+    @Override
+    protected void onStart() {
+// Always call the superclass method first
+        super.onStart();
+        Log.i("","onStart() Message on.");
+
+    }
+    @Override
+    public void onPause() {
+// Always call the superclass method first
+        super.onPause();
+        Log.i("","onPause() Message on.");
+
+    }
+    @Override
+    public void onStop() {
+// Always call the superclass method first
+        super.onStop();
+        Log.i("","onStop() Message on.");
+
+    }
+    @Override
+    public void onDestroy() {
+// Always call the superclass method first
+        super.onDestroy();
+        Log.i("","onDestroy() Message on.");
+
+    }
+}
+
